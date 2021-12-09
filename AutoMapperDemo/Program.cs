4d6d5c3a72e297a7fb954834c65c7dce4ad58bb9
shivanikaktikar <shivanikaktikar@gmail.com>
2021-12-09 15:17:44 +0530

    Contain C# project of AutoMapper Example Scenario 1

---
+++ AutoMapperDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using AutoMapper;
+
+namespace AutoMapperDemo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Initialize the mapper
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Employee, EmployeeDTO>());
+
+            //Creating the source object
+            Employee emp = new Employee
+            {
+                Name = "ABC",
+                Salary = 15000,
+                Address = "Pune",
+                Department = "Healthcare"
+            };
+
+            //using Automapper
+            var mapper = new Mapper(config);
+            var empDTO = mapper.Map<EmployeeDTO>(emp);
+
+            Console.WriteLine("Name:" +empDTO.Name + ", Salary:" +empDTO.Salary +",Address:" +empDTO.Address +", Department" + empDTO.Department);
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        public string Name {get; set;}
+        public int Salary {get; set;}
+        public string Address {get;set;}
+        public string Department {get;set;}
+    }
+
+    public class EmployeeDTO
+    {
+        public string Name {get; set;}
+        public int Salary {get; set;}
+        public string Address {get;set;}
+        public string Department {get;set;}
+    }
+}
